9a7cef31c56862dcb6ee86bef45a93df352c36c5
Kagol <kagol@sina.com>
2026-02-04 19:31:21 +0800

    feat: add univer sheets

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import DemoUniverSheets from './components/DemoUniverSheets.vue'
+</script>
+
+<template>
+  <DemoUniverSheets />
+</template>
---
+++ src/components/DemoUniverSheets.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import { onMounted } from 'vue'
+import { UniverSheetsCorePreset } from '@univerjs/preset-sheets-core'
+import UniverPresetSheetsCoreZhCN from '@univerjs/preset-sheets-core/locales/zh-CN'
+import { createUniver, LocaleType, mergeLocales } from '@univerjs/presets'
+
+import '@univerjs/preset-sheets-core/lib/index.css'
+
+onMounted(() => {
+
+const { univer, univerAPI } = createUniver({
+  locale: LocaleType.ZH_CN,
+  locales: {
+    [LocaleType.ZH_CN]: mergeLocales(
+      UniverPresetSheetsCoreZhCN,
+    ),
+  },
+  presets: [
+    UniverSheetsCorePreset({
+      container: 'app-univer-sheet',
+    }),
+  ],
+})
+
+univerAPI.createWorkbook({})
+})
+</script>
+
+<template>
+    <div id="app-univer-sheet"></div>
+</template>
+
+<style>
+html,
+body,
+#app,
+#app-univer-sheet {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+</style>
